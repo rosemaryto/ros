d56d738eb072d86b13763a4f2d2db4a9652da9d9
rosemaryto <rosemary_to@ymail.com>
2021-02-17 23:43:12 -0800

    thru17

--- script.js
+++ script.js
@@ -5,4 +5,94 @@
 // let summerMonth;
 // let myLibraryFunction;
 
+// STEP 2
+// 5, "hello!", true, null
+
+// STEP 3
+// let makeMath = 3 * 2 (3 / 6) - 1;
+// var someStuff = 1;
+
+// STEP 4
+// let firstName;
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let zipCode;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+// STEP 5
+// let firstName = "Rosemary";
+// let lastName;
+// lastName = "Rosemary";
+// let city = "san Diego", yourAge = "37";
+
+//STEP 6
+// var carValue = 5;
+// var carName = "red";
+// window.console.log(carValue + carName);
+
+//STEP 7
+// var thingOne;
+// var thingTwo;
+// thingOne = true + "Hi!";
+// window.console.log(thingOne);
+// thingTwo = 5 + false;
+// window.console.log(thingTwo);
+
+//STEP 8
+//no
+// let someString = ('Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."\ ');
+// window.console.log(someString);
+
+// STEP 9
+// var meFun = null;
+// window.console.log(meFun);
+// var funMe;
+// window.console.log(funMe);
+
+//STEP 10
+// var first = false;
+// var seco = 2;
+// var stf = "hello";
+// var candy;
+// window.console.log(typeof(first));
+// window.console.log(typeof(seco));
+// window.console.log(typeof(stf));
+// window.console.log(typeof(candy));
+
+//STEP 11
+// var nameUr = "Rosemary";
+// alert("Hello " + nameUr + ", welcome to the JavaScript Class!");
+
+//STEP 12
+// var name = "Rosemary To";
+// alert("Hello " + name + ", welcome to the JavaScript Class!");
+
+// //STEP 13
+// var course = "JavaScript";
+// alert("Hello " + name + ", welcome to the " + course + "!");
+
+//STEP 14
+// var course = "JavaScript";
+// document.write("Hello " + name + "<br>" + "Welcome to the " + course + "!");
+
+//STEP 15
+// var myName;
+// myName = window.prompt("what is your name?");
+// window.console.log(myName);
+
+//STEP 16
+// var course;
+// course = window.prompt("what is your name?");
+// window.console.log(course);
+
+// //STEP 17
+// var x = 10;
+// var y = 20;
+// var total
+// total = (x + y);
+// window.console.log(total);
 
